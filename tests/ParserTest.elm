a2cbaa5c8fd1fd37d4b21b86597dab3bd7e16d4f
AIRTucha <airtucha@icloud.com>
2017-12-22 09:13:28 +0200

    some extra tests

--- tests/ParserTest.elm
+++ tests/ParserTest.elm
@@ -108,5 +108,34 @@ suite =
                     "9" ++ "&3.1415"
                         |> parser ( int <&> float)
                         |> Expect.equal ( MultyValue <| (Interger 9) :: (Floating 3.1415 ) :: [] )
+            , test "string, float and int" <|
+                \_ ->
+                    str ++ "/9" ++ "&3.1415"
+                        |> parser ( p str </> int <&> float)
+                        |> Expect.equal ( MultyValue <| (Interger 9) :: (Floating 3.1415 ) :: [] )
+            , test "two strings, int and float" <|
+                \_ ->  
+                    str ++ "/3.1415" ++ "&9?" ++ str 
+                        |> parser ( p str </> float <&> int <?> p str )
+                        |> Expect.equal ( MultyValue <| (Floating 3.1415 ) :: (Interger 9) :: [] )
+            , test "two strings, int and float mixed" <|
+                \_ ->
+                    let
+                        str1 = str ++ "1"
+                        str2 = str ++ "2"
+                    in
+                        str1 ++ "/3.1415?" ++ str2 ++ "&9"
+                            |> parser ( p str1 </> float <?> p str2 <&> int )
+                            |> Expect.equal ( MultyValue <| (Floating 3.1415 ) :: (Interger 9) :: [] )
+            , test "three strings, int and float mixed" <|
+                \_ ->
+                    let
+                        str1 = str ++ "1"
+                        str2 = str ++ "2"
+                        str3 = str ++ "3"
+                    in
+                        str1 ++ "/3.1415?" ++ str2 ++ "&9/" ++ str3
+                            |> parser ( p str1 </> float <?> p str2 <&> int </> p str3 )
+                            |> Expect.equal ( MultyValue <| (Floating 3.1415 ) :: (Interger 9) :: [] )
             ]
         ]
