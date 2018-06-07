f3de09f95db388c888aa5572a3fc3402eb100e0e
AIRTucha <airtucha@icloud.com>
2018-06-07 23:34:07 +0300

    added Router.Internals test structure in comments

--- tests/Board/Router/InternalsTest.elm
+++ tests/Board/Router/InternalsTest.elm
@@ -11,19 +11,43 @@ import Board.Internals exposing(..)
 -}
 param : Test
 param =
-    describe "Factory"
-        [ test "test" <|
-            \_ ->
-                let 
-                    handler = (\(params, req) -> Next req)
-                    req = request Get
-                in
-                    router stateLessSync anyMethod any handler empty req
-                        |> Expect.equal (nextStateLessSync req)
-        -- Check Method is correct
-        -- URL match
-        -- URL does not match
+    describe "Router Interlan logic"
+        [ describe "Sync State Router"
+            [ 
+                test "Pass" <|
+                    \_ -> 
+                        let
+                            handler = \(params, req) -> Next req 
+                            req = request Get
+                        in
+                            router stateLessSync anyMethod any handler empty req
+                                |> Expect.equal (nextStateLessSync req)
+            ]
+        , describe "Sync Router"
+            [ 
+                test "place holder 1" <|
+                    \_ -> 
+                        Expect.equal True True
+            ],
+            test "place holder 2" <|
+                \_ ->
+                    let
+                        handler = (\(params, req) -> Next req)
+                        req = request Get
+                    in
+                        router stateLessSync anyMethod any handler empty req
+                            |> Expect.equal (nextStateLessSync req)
+        -- Different handlers
+            -- Different router types
+                -- Check router redirect
+                -- Check router next
+                -- Check router respons
+                -- Check that handler redirect
+                -- Check that handler next
+                -- Check that handler respons
+                -- Check Method fails
+                -- Check Method works
+                -- Check URL fails
+                -- Check URL works
         -- Multiple handlers
-        -- Mode is applied
-        -- Async mode staies Async
         ]
