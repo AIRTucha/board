d7a05e1504e6ab3f0496003046027e9c3a8eea66
AIRTucha <airtucha@icloud.com>
2019-01-25 00:30:24 +0300

    some functiona are moved to closures

--- tests/Board/Router/InternalsTest.elm
+++ tests/Board/Router/InternalsTest.elm
@@ -9,33 +9,6 @@ import List exposing (concat, map)
 import Task exposing (succeed, andThen)
 
 
-jounMethodToChecker : ( a, String ) -> ( b, String ) -> ( a, b, String )
-jounMethodToChecker (checker, checkerName) (method, methodName) =
-  (checker, method, "Expects " ++ checkerName ++ ", received " ++ methodName)
-
-
-joinCheckerToMethods : ( a, String ) -> List ( a, Method, String )
-joinCheckerToMethods checkerTuple =
-  [ ( Get, "Get" )
-  , ( Post, "Post" )
-  , ( Put, "Put" )
-  , ( Delete, "Delete" )
-  ]
-    |> map (jounMethodToChecker checkerTuple)
-
-
-methodTestCases : List ( Request a -> Bool, Method, String )
-methodTestCases =
-  [ ( isGet, "Get" )
-  , ( isPost, "Post" )
-  , ( isPut, "Put" )
-  , ( isDelete, "Delete" )
-  , ( anyMethod, "Any" )
-  ] 
-    |> map joinCheckerToMethods
-    |> concat
-
-
 url : String
 url = "test"
 
@@ -56,31 +29,6 @@ next req str =
 redirect : a -> String -> AnswerValue value model error
 redirect req str =
     Redirect str
-
-
-getHandler : (Request value -> String -> AnswerValue value model error) -> ( Params, Request value ) -> AnswerValue value model error
-getHandler hanlder (param, req) =
-    case param of
-        StrParam str ->
-            hanlder req str
-        
-        _ -> 
-            Next req
-
-
-toResponse : ( Params, Request a1 ) -> AnswerValue a1 model error
-toResponse =
-    getHandler response
-
-
-toNext : ( Params, Request a ) -> AnswerValue a model error
-toNext = 
-    getHandler next
-
-
-toRedirect : ( Params, Request value ) -> AnswerValue value model error
-toRedirect =
-    getHandler redirect
 
 
 stateLessSyncNext : Request value -> Mode error1 (Answer value model error)
@@ -164,65 +112,7 @@ equal v1 v2 =
 toStateFullHanlder : (a -> b) -> a -> c -> ( c, b )
 toStateFullHanlder handler args model =
     (model, handler args)
-
-
-stateFullNext : ( Params, Request a ) -> c -> ( c, AnswerValue a model error )
-stateFullNext = 
-    toStateFullHanlder toNext
-
-
-stateFullRedirect : ( Params, Request value ) -> c -> ( c, AnswerValue value model error )
-stateFullRedirect =
-    toStateFullHanlder toRedirect
-
-
-stateFullResponse : ( Params, Request a1 ) -> c -> ( c, AnswerValue a1 model error )
-stateFullResponse =
-    toStateFullHanlder toResponse
-
-
-runTests router chekcer method req handlersMode cases =
-    let 
-        handlerResult = cases.handlerResult req chekcer 
-    in
-        describe cases.name
-            [ test "Router Response" (   
-                router chekcer any (handlersMode toNext) (cases.responseRoute) req
-                    |> equal (cases.responseRoute req)
-              )
-            , test "Router Redirect" (
-                router chekcer any (handlersMode toNext) (cases.redirectRoute) req
-                    |> equal (cases.redirectRoute req)
-              )
-            , describe "Router Next"
-                [ test "Handler Redirect" ( 
-                    router chekcer str (handlersMode toRedirect) cases.nextRoute req
-                        |> equal (handlerResult redirect )
-                  )
-                , test "Handler Reply" ( 
-                    router chekcer str (handlersMode toResponse) cases.nextRoute req
-                        |> equal (handlerResult response ) 
-                  ) 
-                , test "Handler Next" (
-                    router chekcer str (handlersMode toNext) cases.nextRoute req
-                        |> equal (handlerResult next )  
-                  ) 
-                , test "Hanler URL does not match" ( 
-                    router chekcer int (handlersMode toNext) cases.nextRoute req
-                        |> equal (cases.nextRoute req)  
-                  )
-                ]
-            ]
-
-
-testCheckerAndMethod cases (chekcer, method, name) =
-  let 
-    req = getRequest method
-  in
-    describe ( cases.name ++ ": " ++ name ) (
-      map (runTests cases.router chekcer method { req | url = url } cases.handlersMode) cases.tests
-    )
-            
+    
 
 toStatelessAsync : AnswerValue value model error1 -> Mode error (Answer value model error1)
 toStatelessAsync =
@@ -264,127 +154,192 @@ toAsyncStateHandler =
     toStateFulRouter (stateLessAsync << succeed)
 
 
-toSyncStateHandlerStateFull : (c -> d -> StateHandler value model error) -> c -> d -> e -> ( e, Mode error1 (Answer value model error) )
-toSyncStateHandlerStateFull = 
-    toStateFulRouter stateFullSync
-
-
-
 toAsyncStateHandlerStateFull mode handler = 
   toStateFulRouter mode (\ a s m -> (m, stateLessSync <| handler a s))
 
-createSyncRouter handlerResult = 
-  { name = "Sync Router"
-  , responseRoute = getResponse >> Reply >> stateLessSync
-  , redirectRoute = \_ -> url
-      |> Redirect
-      |> stateLessSync  
-  , nextRoute = stateLessSyncNext
-  , handlerResult = handlerResult 
-  }
 
-createAsyncRouter handlerResult =
-  { name = "Async Router"
-  , responseRoute = getResponse >> Reply >> toStatelessAsync
-  , redirectRoute = \ _ -> url
-      |> Redirect
-      |> toStatelessAsync
-  , nextRoute = stateLessAsyncNext
-  , handlerResult = handlerResult
-  }
-
-createSyncStateRouter handlerResult = 
-  { name = "Sync State Router"
-  , responseRoute = stateFullSyncRouter response
-  , redirectRoute = stateFullSyncRouter redirect
-  , nextRoute = stateFullSyncNext
-  , handlerResult = handlerResult
-  }
-
-createAsyncStateRouter handlerResult = 
-  { name = "Async State Router"
-  , responseRoute = stateFullAsyncRouter response
-  , redirectRoute = stateFullAsyncRouter redirect
-  , nextRoute = stateFullAsyncNext
-  , handlerResult = handlerResult
-  }
-
-testDescription = 
-    { router = syncRouter
-        , name = "Sync StateLess Handler" 
-        , handlersMode = identity
+createTestDesciption description =
+    let 
+        reply = getResponse >> Reply
+        redirectRoute mode _ = 
+            url
+                |> Redirect
+                |> mode
+    in
+        { router = description.router
+        , name = description.name 
+        , handlersMode = description.handlersMode
         , tests = 
-            [ createSyncRouter <| \ req chekcer -> 
-                result chekcer req stateLessSyncNext stateLessSync 
-            , createAsyncRouter <| \ req chekcer ->
-                result chekcer req stateLessAsyncNext toStatelessAsync
-            , createSyncStateRouter <| \ req chekcer ->
-                  toSyncStateHandler >> result chekcer req stateFullSyncNext stateFullSync
-            , createAsyncStateRouter <| \ req chekcer ->
-                  toSyncStateHandler >> result chekcer req stateFullAsyncNext stateFullAsync
+            [ { name = "Sync Router"
+              , responseRoute = reply >> stateLessSync
+              , redirectRoute = redirectRoute stateLessSync  
+              , nextRoute = stateLessSyncNext
+              , handlerResult = description.syncRouterResultHandler 
+              }
+            , { name = "Async Router"
+              , responseRoute = reply >> toStatelessAsync
+              , redirectRoute = redirectRoute toStatelessAsync
+              , nextRoute = stateLessAsyncNext
+              , handlerResult = description.asyncRouterResultHandler 
+              }
+            , { name = "Sync State Router"
+              , responseRoute = stateFullSyncRouter response
+              , redirectRoute = stateFullSyncRouter redirect
+              , nextRoute = stateFullSyncNext
+              , handlerResult = description.syncStateRouterResultHandler 
+              }
+            , { name = "Async State Router"
+              , responseRoute = stateFullAsyncRouter response
+              , redirectRoute = stateFullAsyncRouter redirect
+              , nextRoute = stateFullAsyncNext
+              , handlerResult = description.asyncStateRouterResultHandler 
+              }
             ]
         }
 
+
+testDescription = 
+    createTestDesciption
+        { router = syncRouter
+        , name = "Sync StateLess Handler" 
+        , handlersMode = identity
+        , syncRouterResultHandler = \ req chekcer -> 
+            result chekcer req stateLessSyncNext stateLessSync 
+        , asyncRouterResultHandler = \ req chekcer ->
+            result chekcer req stateLessAsyncNext toStatelessAsync
+        , syncStateRouterResultHandler =  \ req chekcer ->
+            toSyncStateHandler >> result chekcer req stateFullSyncNext stateFullSync
+        , asyncStateRouterResultHandler = \ req chekcer ->
+            toSyncStateHandler >> result chekcer req stateFullAsyncNext stateFullAsync
+        }
+
 testDescription2 =
+    createTestDesciption
         { router = asyncRouter
         , name = "Async StateLess Handler" 
         , handlersMode = \ f -> f >> succeed
-        , tests = 
-            [ createSyncRouter <| \ req chekcer ->
-                result chekcer req stateLessSyncNext toStatelessAsync
-            , createAsyncRouter <| \ req chekcer ->
-                result chekcer req stateLessAsyncNext toStatelessAsync
-            , createSyncStateRouter <| \ req chekcer ->
-                toAsyncStateHandler >> result chekcer req stateFullSyncNext stateFullSync
-            , createAsyncStateRouter <| \ req chekcer ->
-                  toAsyncStateHandler >> result chekcer req stateFullAsyncNext stateFullAsync
-            ]
+        , syncRouterResultHandler = \ req chekcer ->
+            result chekcer req stateLessSyncNext toStatelessAsync
+        , asyncRouterResultHandler = \ req chekcer ->
+            result chekcer req stateLessAsyncNext toStatelessAsync
+        , syncStateRouterResultHandler = \ req chekcer ->
+            toAsyncStateHandler >> result chekcer req stateFullSyncNext stateFullSync
+        , asyncStateRouterResultHandler = \ req chekcer ->
+            toAsyncStateHandler >> result chekcer req stateFullAsyncNext stateFullAsync
         }
 
 
 testDescription3 =
+    createTestDesciption
         { router = syncStateRouter
         , name = "Sync StateFul Handler" 
         , handlersMode = toStateFullHanlder
-        , tests = 
-            [ createSyncRouter <| \ req chekcer ->
-                toSyncStateHandler >> result chekcer req stateLessSyncNext stateFullSync
-            , createAsyncRouter <| \ req chekcer ->
-                  toSyncStateHandler >> result chekcer req stateLessAsyncNext stateFullAsync
-            , createSyncStateRouter <| \ req chekcer ->
-                  toStateFulRouter stateLessSync >> toSyncStateHandlerStateFull >> result chekcer req stateFullSyncNext stateFullSync 
-            , createAsyncStateRouter <| \ req chekcer ->
-                  toAsyncStateHandlerStateFull stateFullSync >> result chekcer req stateFullAsyncNext stateFullAsync
-            ]
+        , syncRouterResultHandler = \ req chekcer ->
+            toSyncStateHandler >> result chekcer req stateLessSyncNext stateFullSync
+        , asyncRouterResultHandler = \ req chekcer ->
+            toSyncStateHandler >> result chekcer req stateLessAsyncNext stateFullAsync
+        , syncStateRouterResultHandler = \ req chekcer ->
+            toStateFulRouter stateLessSync >> (toStateFulRouter stateFullSync) >> result chekcer req stateFullSyncNext stateFullSync 
+        , asyncStateRouterResultHandler = \ req chekcer ->
+            toAsyncStateHandlerStateFull stateFullSync >> result chekcer req stateFullAsyncNext stateFullAsync
         }
 
 
 testDescription4 =
-  let
-    toAsyncStateHandlerAsyncStateFull = toAsyncStateHandlerStateFull stateFullAsync
-  in
-    { router = asyncStateRouter
-    , name = "Async StateFul Handler" 
-    , handlersMode = \ f -> (toStateFullHanlder f) >> succeed
-    , tests = 
-        [ createSyncRouter <| \ req chekcer ->
-            toSyncStateHandler >> result chekcer req stateLessSyncNext stateFullAsync
-        , createAsyncRouter <| \ req chekcer ->
-            toSyncStateHandler >> result chekcer req stateLessAsyncNext stateFullAsync
-        , createSyncStateRouter <| \ req chekcer ->
-              toAsyncStateHandlerAsyncStateFull >> result chekcer req stateFullSyncNext stateFullSync 
-        , createAsyncStateRouter <| \ req chekcer ->
-              toAsyncStateHandlerAsyncStateFull >> result chekcer req stateFullAsyncNext stateFullAsync
-        ]
-    }
+    let
+        toAsyncStateHandlerAsyncStateFull = toAsyncStateHandlerStateFull stateFullAsync
+    in
+        createTestDesciption
+            { router = asyncStateRouter
+            , name = "Async StateFul Handler" 
+            , handlersMode = \ f -> (toStateFullHanlder f) >> succeed
+            , syncRouterResultHandler = \ req chekcer ->
+                toSyncStateHandler >> result chekcer req stateLessSyncNext stateFullAsync
+            , asyncRouterResultHandler = \ req chekcer ->
+                toSyncStateHandler >> result chekcer req stateLessAsyncNext stateFullAsync
+            , syncStateRouterResultHandler = \ req chekcer ->
+                toAsyncStateHandlerAsyncStateFull >> result chekcer req stateFullSyncNext stateFullSync 
+            , asyncStateRouterResultHandler = \ req chekcer ->
+                toAsyncStateHandlerAsyncStateFull >> result chekcer req stateFullAsyncNext stateFullAsync
+            }
 
+
+testCheckerAndMethod {router, name, handlersMode, tests } (chekcer, method, confName) =
+  let 
+    req = getRequest method
+    runTests req cases =
+        let 
+            handlerResult = cases.handlerResult req chekcer 
+            getHandler hanlder (param, req) =
+                case param of
+                    StrParam str ->
+                        hanlder req str
+                    
+                    _ -> 
+                        Next req
+            toNext = 
+                handlersMode <| getHandler next
+        in
+            describe cases.name
+                [ test "Router Response" (   
+                    router chekcer any toNext (cases.responseRoute) req
+                        |> equal (cases.responseRoute req)
+                )
+                , test "Router Redirect" (
+                    router chekcer any toNext (cases.redirectRoute) req
+                        |> equal (cases.redirectRoute req)
+                )
+                , describe "Router Next"
+                    [ test "Handler Redirect" ( 
+                        router chekcer str (handlersMode <| getHandler redirect) cases.nextRoute req
+                            |> equal (handlerResult redirect )
+                    )
+                    , test "Handler Reply" ( 
+                        router chekcer str (handlersMode <| getHandler response) cases.nextRoute req
+                            |> equal (handlerResult response ) 
+                    ) 
+                    , test "Handler Next" (
+                        router chekcer str toNext cases.nextRoute req
+                            |> equal (handlerResult next )  
+                    ) 
+                    , test "Hanler URL does not match" ( 
+                        router chekcer int toNext cases.nextRoute req
+                            |> equal (cases.nextRoute req)  
+                    )
+                    ]
+                ]
+  in
+    describe ( name ++ ": " ++ confName ) (
+      map ( runTests { req | url = url } ) tests
+    )
+        
 
 {-
 -}
 routerInternals : Test
 routerInternals =
-    describe "Router Interlan logic" <|
-      (map (testCheckerAndMethod testDescription) methodTestCases) ++
-      (map (testCheckerAndMethod testDescription2) methodTestCases) ++
-      (map (testCheckerAndMethod testDescription3) methodTestCases) ++ 
-      (map (testCheckerAndMethod testDescription4) methodTestCases)
+    let 
+        jounMethodToChecker (checker, checkerName) (method, methodName) =
+            (checker, method, "Expects " ++ checkerName ++ ", received " ++ methodName)
+        joinCheckerToMethods checkerTuple =
+            [ ( Get, "Get" )
+            , ( Post, "Post" )
+            , ( Put, "Put" )
+            , ( Delete, "Delete" )
+            ]
+                |> map (jounMethodToChecker checkerTuple)
+        methodTestCases =
+            [ ( isGet, "Get" )
+            , ( isPost, "Post" )
+            , ( isPut, "Put" )
+            , ( isDelete, "Delete" )
+            , ( anyMethod, "Any" )
+            ] 
+                |> map joinCheckerToMethods
+                |> concat
+    in
+        describe "Router Interlan logic" <|
+            (map (testCheckerAndMethod testDescription) methodTestCases) ++
+            (map (testCheckerAndMethod testDescription2) methodTestCases) ++
+            (map (testCheckerAndMethod testDescription3) methodTestCases) ++ 
+            (map (testCheckerAndMethod testDescription4) methodTestCases)
